bf59cdf130f2855a1cc35ff47e8ccfd6bf5c3461
Tommy-Amen <amenlemaht@gmail.com>
2025-03-10 21:27:06 +0000

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import Services from "./Services";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+// import Nav from "./sections/Nav";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  Services,
+  CustomerReviews,
+  Footer,
+  Subscribe,
+  SpecialOffer,
+  PopularProducts,
+  SuperQuality,
+}; //, Nav };
