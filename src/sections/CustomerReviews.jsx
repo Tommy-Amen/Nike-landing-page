c01e30ed0969737ea7415754c09007ca57452c07
Tommy-Amen <amenlemaht@gmail.com>
2025-03-12 20:10:49 +0000

    updated components

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,3 +1,5 @@
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
 
 const CustomerReviews = () => {
   return (
@@ -8,6 +10,18 @@ const CustomerReviews = () => {
       <p className="info-text m-auto mt-4 max-w-lg">
         Read our testimonials from satisfied customers to inspire others about our high-quality footwear.
       </p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
     </section>
   );
 }
